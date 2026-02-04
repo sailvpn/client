552c34bfbfe7e2680e5f03f66750910772882705
jing-zhou <jingzhou_wang@hotmail.com>
2026-02-04 12:32:05 +0800

    ProxyApplication: resource leakage detector

--- src/main/java/com/illiad/proxy/ProxyApplication.java
+++ src/main/java/com/illiad/proxy/ProxyApplication.java
@@ -1,5 +1,6 @@
 package com.illiad.proxy;
 
+import io.netty.util.ResourceLeakDetector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +9,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.illiad.proxy"})
 public class ProxyApplication {
 	public static void main(String[] args) {
+		// direct memory leakage test
+		// ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
 		SpringApplication.run(ProxyApplication.class, args);
 	}
 
