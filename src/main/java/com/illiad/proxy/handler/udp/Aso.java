8aed11bad26ca97afceae487bebd064868e22712
jing-zhou <jingzhou_wang@hotmail.com>
2026-07-03 13:45:40 +0800

    Aso: ByteBuf queue

--- src/main/java/com/illiad/proxy/handler/udp/Aso.java
+++ src/main/java/com/illiad/proxy/handler/udp/Aso.java
@@ -1,5 +1,6 @@
 package com.illiad.proxy.handler.udp;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.util.ReferenceCountUtil;
@@ -19,7 +20,7 @@ public class Aso {
     private Channel forward;
 
     // A thread-safe queue to buffer packets while the upstream handshake is in-flight
-    private final Queue<DatagramPacket> packetBuffer = new ConcurrentLinkedQueue<>();
+    private final Queue<ByteBuf> packetBuffer = new ConcurrentLinkedQueue<>();
 
     private final AtomicBoolean cleaned = new AtomicBoolean(false);
 
@@ -37,7 +38,7 @@ public class Aso {
         }
 
         // Release any stashed buffers to prevent severe direct memory leaks
-        DatagramPacket bufferedPacket;
+        ByteBuf bufferedPacket;
         while ((bufferedPacket = packetBuffer.poll()) != null) {
             ReferenceCountUtil.release(bufferedPacket);
         }
