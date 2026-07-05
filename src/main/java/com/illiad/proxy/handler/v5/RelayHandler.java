aa5ba5d793ed4bf3be43da931a63bd9412cbcae2
jing-zhou <jingzhou_wang@hotmail.com>
2026-07-06 02:34:52 +0800

    RelayHandler: minor changes

--- src/main/java/com/illiad/proxy/handler/v5/RelayHandler.java
+++ src/main/java/com/illiad/proxy/handler/v5/RelayHandler.java
@@ -1,7 +1,6 @@
 package com.illiad.proxy.handler.v5;
 
 import com.illiad.proxy.ParamBus;
-import com.illiad.proxy.handler.Utils;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
